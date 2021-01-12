a5cb8ac1278f5ea99f19cf3defbf01f14ae5e150
KS9004 <uic.19mca8068@gmail.com>
2021-01-12 12:38:28 +0530

    added some text

--- lib/home_page2.dart
+++ lib/home_page2.dart
@@ -12,17 +12,78 @@ class _SecondScreenState extends State<SecondScreen> {
       backgroundColor: Color(0xffE9E9FE),
       body: SafeArea(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Center(child: Image.asset('assets/santa2.png')),
             SizedBox(
               height: 20.0,
             ),
-            Text(
-              "Dear, Santa Claus",
-              style: TextStyle(
-                  color: Color(0xffefb570),
-                  fontSize: 40.0,
-                  fontWeight: FontWeight.bold),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0),
+              child: Text(
+                "Dear, Santa Claus",
+                style: TextStyle(
+                    color: Color(0xffefb570),
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0),
+              child: RichText(
+                text: TextSpan(
+                    text: "My name is ",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                        fontSize: 20.0),
+                    children: [
+                      TextSpan(
+                        text: "Mariam\n",
+                        style: TextStyle(
+                          decoration: TextDecoration.underline,
+                          color: Color(0xff7077eb),
+                        ),
+                      ),
+                      TextSpan(
+                        text: "I'm ",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                            fontSize: 20.0),
+                      ),
+                      TextSpan(
+                        text: "19 ",
+                        style: TextStyle(
+                          decoration: TextDecoration.underline,
+                          color: Color(0xff7077eb),
+                        ),
+                      ),
+                      TextSpan(
+                        text: "years old ",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                            fontSize: 20.0),
+                      ),
+                    ]),
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0),
+              child: Text(
+                "I'm interested in",
+                style: TextStyle(
+                    color: Color(0xffefb570),
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold),
+              ),
             )
           ],
         ),
